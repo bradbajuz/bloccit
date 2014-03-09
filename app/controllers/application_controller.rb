8ea6b181f9842d66475847f2831c51a188c0a933
bradbajuz <brad@bradballard.com>
2014-03-09 12:11:10 -0400

    Added image to Post.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def add_allowed_devise_session_params
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :avatar, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:image, :name, :avatar, :email, :password, :password_confirmation, :current_password) }
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
 private
 
   def post_params
-    params.require(:post).permit(:title, :body, :topic)
+    params.require(:post).permit(:title, :body, :topic, :image)
   end
 end
