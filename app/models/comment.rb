647856d2c9736aadcf75839b1c1be801ef5cc305
bradbajuz <brad@bradballard.com>
2014-03-12 22:48:33 -0400

    Remove commments table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,3 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  belongs_to :user
-
-  default_scope order('created_at DESC')
-
-  validates :body, length: { minimum: 5 }, presence: true
-  validates :user, presence: true
 end
