9b9c0afa9cfcf41ad4b29ba20c8d5a19098af917
bradbajuz <brad@bradballard.com>
2014-03-15 21:50:47 -0400

    Interlude changes for registration, email permissions and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   rescue_from Pundit::NotAuthorizedError do |exception|
     redirect_to root_url, alert: exception.message
   end
 
-  before_filter :add_allowed_devise_session_params, if: :devise_controller?
-
-  protected
-
-  def add_allowed_devise_session_params
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :avatar, :provider, :uid) }
+  def after_sign_in_path_for(resource)
+    topics_path
   end
+
 end
