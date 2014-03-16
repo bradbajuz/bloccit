c60758b269cb7b235db66b83c98b8b5a7546b8e0
bradbajuz <brad@bradballard.com>
2014-03-15 22:07:47 -0400

    Added devise back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     topics_path
   end
+
+  before_filter :add_allowed_devise_session_params, if: :devise_controller?
+
+  protected
+
+  def add_allowed_devise_session_params
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :avatar, :provider, :uid) }
+  end
   
 end
